079571e43fa1e2b3db4e06f0e3f7141c19213c43
aldriclee <leeslingshot@hotmail.com>
2016-10-22 21:15:43 -0500

    First Rep

---
+++ Node.cpp
@@ -0,0 +1,18 @@
+#include "Node.h";
+
+//1) Constructor takes in pointer called mypointer
+//2. this->Left is being initialized with pinter called mypointer
+
+	Node::Node(Node *left) {
+	this->number = 0;
+	this->loft=mypointer;
+	this->right=nullptr;
+
+}
+
+
+
+void Node::add(){
+
+
+}
---
+++ Node.h
@@ -0,0 +1,12 @@
+class Node
+{
+public:
+// constructor
+	int number;
+	Node * left;
+	Node *right;
+	Node(Node * left);
+	void add();
+private:
+
+};
